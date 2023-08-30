6ddeb8839516b90a8339d7bf8fac08699add2f86
zaidmaniyarr <130126107+zaidmaniyarr@users.noreply.github.com>
2023-08-30 15:24:32 +0530

    Add files via upload

---
+++ Royal enfield (html,css, javascript)/RE.js
@@ -0,0 +1,79 @@
+// I have missed the previous section
+
+function a(){
+    document.getElementById("pp").src="./images/1image.avif"
+}
+
+function b(){
+    document.getElementById("pp").src="./images/2image.avif" 
+}
+
+function c(){
+    document.getElementById("pp").src="./images/3image.avif" 
+}
+
+function d(){
+    document.getElementById("pp").src="./images/4image.avif" 
+}
+
+function e(){
+    document.getElementById("pp").src="./images/5image.avif" 
+}
+
+function f(){
+    document.getElementById("pp").src="./images/6image.avif" 
+}
+
+function g(){
+    document.getElementById("pp").src="./images/7image.avif" 
+}
+
+function h(){
+    document.getElementById("pp").src="./images/8image.avif" 
+}
+
+
+// Rides ke liye hai
+
+function A(){
+    document.getElementById("aa").src="./images/Rides1.avif"
+}
+
+function B(){
+    document.getElementById("aa").src="./images/Rides2.avif"
+}
+
+function C(){
+    document.getElementById("aa").src="./images/Rides3.avif"
+}
+
+function D(){
+    document.getElementById("aa").src="./images/Rides4.avif"
+}
+
+
+//Apperel le liye
+
+function a(){
+    document.getElementById("bb").src="./images/Appare1.avif"
+}
+
+function b(){
+    document.getElementById("bb").src="./images/Appare2.avif"
+}
+
+function c(){
+    document.getElementById("bb").src="./images/Appare3.avif"
+}
+
+function d(){
+    document.getElementById("bb").src="./images/Appare4.avif"
+}
+
+function e(){
+    document.getElementById("bb").src="./images/Appare5.avif"
+}
+
+
+
+
